0873361f39f1bffb511327f995ef590085855e53
rohitware <rohitware99@gmail.com>
2022-12-08 13:13:44 +0530

    Declare And Initialize a variables
    Declare and Initialize a variables and log result on console using coma operators.

---
+++ 01_variable_assign.js
@@ -0,0 +1,21 @@
+let myFullName;
+console.log(myFullName);
+
+myFullName = "Rohit Ware";
+console.log(myFullName);
+
+myFullName = "Rohit Bhimrao Ware";
+console.log(myFullName);
+
+
+let myLove = "I Love Only JS";
+console.log(myLove);
+
+let firstName = "Rohit";
+let lastName = "Ware";
+let age = 32;
+let address = "Dange chowk, Pune";
+let pin_code = 411033;
+let gender = "male";
+let is_married = true;
+console.log(firstName, lastName, age, address, pin_code, gender, is_married);
