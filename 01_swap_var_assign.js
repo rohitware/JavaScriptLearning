591034fba5eb014a002f102f1cdb2372c91487cc
rohitware <rohitware99@gmail.com>
2022-12-10 10:13:56 +0530

    swap using another method.

--- 01_swap_var_assign.js
+++ 01_swap_var_assign.js
@@ -25,3 +25,19 @@ num1 = num1 - (num2 + num3);
 
 console.log("==== After Swap ====");
 console.log(num1, num2, num3);
+
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("==== Before Swap ====");
+console.log(num1, num2, num3);
+
+temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+
+console.log("==== After Swap ====");
+console.log(num1, num2, num3);
+
