353869cbad9c21072c7dfd81d5e6c2e974dc1b23
rohitware <rohitware99@gmail.com>
2022-12-10 10:18:34 +0530

    JavaScript Functions

--- 02_function.js
+++ 02_function.js
@@ -1,21 +1,21 @@
-function display( ) {
-  console.log("My name: Mohit");
-  console.log("My sir name is: Sharma");
-  
+function display() {
+    console.log("My name: Mohit");
+    console.log("My sir name is: Sharma");
+
 }
 
 display(); // Function call or function invocation
 
 // Write a function show which should log you college name
 
-function showDetails(myName){
+function showDetails(myName) {
     console.log("Details", myName);
 }
 showDetails("Mohit Sharma");
 
 
 
-function swapValues(value1, value2){
+function swapValues(value1, value2) {
     console.log("Before Swap", value1, value2);
     var temp = value1;
     value1 = value2;
@@ -27,24 +27,24 @@ swapValues(2, 4);
 swapValues("Kishor", "Hitesh");
 swapValues("You", "Me");
 
-function hello(myName){
+function hello(myName) {
     console.log(myName);
 }
 hello("Sachin");
 
 console.log("Function with arguments and return value");
-function rajuBhaiya(paisa){
-  console.log("Going in market", paisa);
-  console.log("Purchased fresh vegetables", paisa);
-  console.log("Come back");
-  return "Bag of vegetables";
-  
-}
- var bagOfVegetables= rajuBhaiya(100);
- console.log(bagOfVegetables);
- 
+function rajuBhaiya(paisa) {
+    console.log("Going in market", paisa);
+    console.log("Purchased fresh vegetables", paisa);
+    console.log("Come back");
+    return "Bag of vegetables";
 
- function add(val1, val2){
+}
+var bagOfVegetables = rajuBhaiya(100);
+console.log(bagOfVegetables);
+
+
+function add(val1, val2) {
     console.log(val1 + val2);
- }
- add(10, 20);
+}
+add(10, 20);
