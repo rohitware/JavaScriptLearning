f04485717407f34ced1b9ee98479f510c5441a0e
rohitware <rohitware99@gmail.com>
2022-12-11 23:36:10 +0530

    Normal Function Example.

---
+++ 02_normal_function.js
@@ -0,0 +1,6 @@
+function bankDetails(bankName, accountNum, location, pinCode) {
+    console.log(`My bank name is ${bankName} And my account number is ${accountNum}. It is located at ${location}`, pinCode);
+}
+bankDetails("CITI Bank", 3456782345, "pune", 431202);
+bankDetails("Axis Bank", 7856782345, "Mumbai", 441202);
+bankDetails("HDFC Bank", 8956782345, "Pune", 631202);
