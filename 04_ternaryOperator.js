23bf194b0e8c8431ce843c16ff34e5e597fcdb08
rohitware <rohitware99@gmail.com>
2022-12-15 23:22:54 +0530

    #

--- 04_ternaryOperator.js
+++ 04_ternaryOperator.js
@@ -4,27 +4,27 @@ console.log(typeof num1);
 console.log(typeof num2);
 console.log(typeof '100');
 
-var result = num1 == num2 ? "Equal" : "Not Equal";
+var result = num1==num2 ? "Equal" : "Not Equal";
 console.log(`num1==num2==> ${result}`);
 
-var res = num1 === num2 ? "Equal" : "Not Equal";
+var res = num1===num2 ? "Equal" : "Not Equal";
 console.log(`num1===num2==> ${res}`);
 
 // If SSC marks greater than equal to 35 then pass or fail
 
 var sscMarks = 39;
-var resultPassOrFail = sscMarks >= 35 ? "Pass" : "Fail";
+var resultPassOrFail= sscMarks>=35 ? "Pass"  : "Fail";
 
-console.log(`Result is ${resultPassOrFail}`);
+ console.log(`Result is ${resultPassOrFail}`);
 
-var sscResult = function (sscMarks) {
+ var sscResult = function(sscMarks){ 
     // var resultPassOrFail= (sscMarks>=35 && !(sscMarks>100)) ? "Pass"  : "Fail";
     // var resultPassOrFail= (sscMarks>=35 && sscMarks<100) ? "Pass"  : "Fail";
-    var resultPassOrFail = (sscMarks < 0 || sscMarks > 100)
-        ? "In valid Data"
-        : (sscMarks > 35 ? "Pass" : "Fail");
+    var resultPassOrFail= (sscMarks < 0 || sscMarks > 100 ) 
+        ? "In valid Data" 
+        : (sscMarks>35 ? "Pass" : "Fail"); 
     return resultPassOrFail;
-}
+ }
 
 console.log(`Result is 23  ${sscResult(23)}`);// Valid Value
 console.log(`Result is 45  ${sscResult(45)}`);// Valid Value
