69b03927ad289e66448d439e6c6f8d80ca6a6b13
rohitware <rohitware99@gmail.com>
2022-12-16 14:12:33 +0530

    JavaScript Comparison
    JavaScript Comparison

---
+++ comparing.js
@@ -0,0 +1,7 @@
+console.log(`Case1: 0 =='' value: ${0 == ""}`);
+console.log(`Case2: 0 =='0' Value: ${0 == '0'}`);
+console.log(`Case3: 0 == false Value: ${0 == false}`);
+console.log(`Case4: null == undefined Value: ${null == undefined}`);
+console.log(`Case5: 1 == [1] Value: ${1 == [1]}`);
+console.log(`Case6: 1 == true Value: ${1 == true}`);
+console.log(`Case7: 1 == '1' Value: ${1 == '1'}`);
