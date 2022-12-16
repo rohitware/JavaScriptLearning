1ab5b57ea5ccb1045236b7fd12acaba8b21825b7
rohitware <rohitware99@gmail.com>
2022-12-16 23:40:53 +0530

    Control flow statement (If Block).

---
+++ 05_ifBlock.js
@@ -0,0 +1,16 @@
+console.log(` ====== if block ======= `);
+console.log(`Check whether number is positive or negative...`);
+var myNumber = -10;
+if(myNumber>0){
+    console.log(`Given number ${myNumber} is positive`);
+}
+var num = -4;
+if (num<=0) {
+    console.log(`Given number ${num} is Negative`);
+}
+var num1 =100;
+var num2 = 200;
+var num3 = 99;
+if (num1>num2 && num1>num3) {
+    console.log(`You are greater man ${num1}`);
+}
