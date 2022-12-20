286d6c2675d0b4f1f62d59465822c0cdb5468811
rohitware <rohitware99@gmail.com>
2022-12-20 22:02:51 +0530

    For Loop Assignment
    For Loop Assignment

---
+++ 05_forLoopAssign.js
@@ -0,0 +1,64 @@
+for (let index = 5; index <= 15; index++) {
+console.log(index);
+}
+
+console.log(`   =========================`);
+
+for (let index = 50; index >=40; index--) {
+    console.log(index);    
+}
+console.log(`   =========================`);
+
+for (let index = 1; index<=20; index++) {
+    if (index%2!= 0) 
+        // console.log(x +  " is even");
+
+    console.log(index);       
+}
+
+console.log(`   =========================`);
+
+for (let index = 1; index<=20; index++) {
+    if (index%2== 0) 
+        // console.log(x +  " is even");
+
+    console.log(index);        
+}
+console.log(`   =========================`);
+
+var number = 5;
+for(let i = 1; i <= 10; i++) {
+
+    // multiply i with number
+    const result = i * number;
+
+    // display the result
+    console.log(`${number} * ${i} = ${result}`);
+}
+
+console.log(`   =========================`);
+
+var number = 10;
+for(let i = 1; i <= 10; i++) {
+
+    // multiply i with number
+    const result = i * number;
+
+    // display the result
+    console.log(`${number} * ${i} = ${result}`);
+}
+
+
+console.log(`   =========================`);
+
+var number = 10;
+for(let i = 10; i >= 1; i--) {
+
+    // multiply i with number
+    const result = i * number;
+
+    // display the result
+    console.log(`${number} * ${i} = ${result}`);
+}
+
+
