00db6538017df2dd00d44bea39568e4a2748b223
rohitware <rohitware99@gmail.com>
2023-01-01 23:30:00 +0530

    Update 07_functionConstructor.js

--- 07_functionConstructor.js
+++ 07_functionConstructor.js
@@ -27,3 +27,23 @@ console.log(`------------------------------------------------`);
  console.log(minny.country);
  console.log(jenny.country);
  console.log(tommy.country);
+
+
+
+ // Array
+const arrayOfNum = [3, 5, 6, 8, 9];
+let arrayLen = arrayOfNum.length;
+arrayOfNum.push(10);
+arrayOfNum.pop();
+arrayOfNum.unshift()
+
+let date = new Date("1996-12-22");
+let month = date.getMonth();
+let year = date.getFullYear();
+let day = date.getDay();
+console.log(month, year, day);
+
+let sqrt =  Math.sqrt(25);
+let pow = Math.pow(2, 3);//2*2*2
+let pi =  Math.PI;
+console.log(sqrt, pow, pi);
