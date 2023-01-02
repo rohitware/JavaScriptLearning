96ec622ec9a4ef0a9e61a5100859eb1f78948b15
rohitware <rohitware99@gmail.com>
2023-01-02 22:32:43 +0530

    Update 07_objectMergeAssign2.js

--- 07_objectMergeAssign2.js
+++ 07_objectMergeAssign2.js
@@ -11,14 +11,14 @@ const carEngine = {
 
 const obj = Object.assign(car, carEngine);
 console.log(`Merge Using Object.assign: 1 new object, 2 car object, 3 carEngine object`);
-console.log(obj);
-console.log(car);
-console.log(carEngine);
+console.log(`1 new object`, obj);
+console.log(`2 car object`, car);
+console.log(`3 carEngine object`, carEngine);
 console.log(`-------------------------------------------------------------------------------------------`);
 
 let obj1 = { ...car, ...carEngine }
 console.log(`Merge Using Spread Operator: 1 new object, 2 car object, 3 carEngine object`);
-console.log(obj1);
-console.log(car);
-console.log(carEngine);
+console.log(`1 new object`, obj1);
+console.log(`2 car object`, car);
+console.log(`3 carEngine object`, carEngine);
 
