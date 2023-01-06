75834e7dac0cfdb618ad77bb6d95a4147b47b1fa
rohitware <rohitware99@gmail.com>
2023-01-06 08:57:07 +0530

    Create 10_hoisting.js

---
+++ 10_hoisting.js
@@ -0,0 +1,19 @@
+city = "Pune";
+console.log(city);
+
+var city;
+
+
+let state = "MH";
+console.log(state);
+
+show();
+
+function show(){
+    console.log("Hello I am in show");
+}
+
+display();
+var display = function(){
+    console.log("Hello I am in display");
+}
