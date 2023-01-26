82abdf77631ec53cbc922de6211df72734fe3846
rohitware <rohitware99@gmail.com>
2023-01-26 22:12:35 +0530

    practice

---
+++ countValues.js
@@ -0,0 +1,18 @@
+const response = {
+    maran: {
+        count: 1,
+    },
+    gas: {
+        count: 2,
+    },
+    prelude: {
+        count: 5,
+    },
+    keys: ['maran', 'gas', 'prelude'],
+};
+
+let sum = 0;
+for (let key of response.keys) {
+    sum = response[key].count + sum;
+}
+console.log(`Addition of all count values is:`, sum);
